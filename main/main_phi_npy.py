072b15da3839cc6d34756670a88db6c9fe0cfbb3
Satria Widyanto <s2601026@t002.ph.ed.ac.uk>
2026-06-16 18:03:07 +0100

    fix bugs in dirname

--- main/main_phi_npy.py
+++ main/main_phi_npy.py
@@ -103,7 +103,7 @@ def main(args):
                                         args.bs, args.num_noise, args.num_bootstrap)
 
         npy_dir = join_paths(args.main_dir, f"results/npy/L{args.L}_inteval{args.integrator_eval}_integrator{args.integrator}_dt{args.dt}_bs{args.bs}_noise_{args.num_noise}")
-        os.makedirs(os.path.dirname(npy_dir), exist_ok=True)
+        os.makedirs(os.path.dirname(join_paths(npy_dir, f"susc.npy")), exist_ok=True)
         np.save(f'{npy_dir}/susc.npy', susc.detach().cpu().numpy())
         np.save(f'{npy_dir}/logw.npy', logw.detach().cpu().numpy())
         np.save(f'{npy_dir}/twop.npy', twop.detach().cpu().numpy())
